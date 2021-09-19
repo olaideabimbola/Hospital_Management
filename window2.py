71db973c38e4be1d3ea9d112f68f361e21807bc4
olaideabimbola <olaideabimbola2@gmail.com>
2021-09-19 12:25:16 +0200

    update

--- window2.py
+++ window2.py
@@ -10,12 +10,12 @@ class window2:
         self.p_frame.grid()
 
         #=================================Variable declaration===============================#
-        VrefNo = StringVar()
-        VfName = StringVar()
-        VlName = StringVar()
-        VtelNo = StringVar()
-        Vaddr = StringVar()
-        VpCode = StringVar()
+        self.VrefNo = StringVar()
+        self.VfName = StringVar()
+        self.VlName = StringVar()
+        self.VtelNo = StringVar()
+        self.Vaddr = StringVar()
+        self.VpCode = StringVar()
         #======================================================================================#
 
 
@@ -45,44 +45,51 @@ class window2:
         self.display_details = Label(self.sub_inn_frame2, font=('arial','9','bold'),
                                      text='Reference No.\t First Name\t Surname\t\t Address\t\t Telephone No \t Post Code')
         self.display_details.grid( pady=10, padx=15, columnspan=4)
+
+        self.textReceipt = Text(self.sub_inn_frame2, font=('arial','9','bold'), width=97, height=30)
+        self.textReceipt.grid(row=1, column=0, columnspan=4)
         #=======================#
 
         self.refNo = Label(self.sub_inn_frame1, text = 'Reference No.', font=('arial', 11, 'bold'))
         self.refNo.grid(row=0, column=0, pady=10, sticky=W)
-        self.refNo_entry =Entry(self.sub_inn_frame1, font=('arial', 10, 'bold'), bd=4, insertwidth=2, textvariable=VrefNo)
+        self.refNo_entry =Entry(self.sub_inn_frame1, font=('arial', 10, 'bold'), bd=4, insertwidth=2, textvariable=self.VrefNo)
         self.refNo_entry.grid(row=0, column=1, padx=10)
 
         self.fName = Label(self.sub_inn_frame1, text = 'First Name.', font=('arial', 11, 'bold'))
         self.fName.grid(row=1, column=0, sticky=W,pady=10)
-        self.fName_entry =Entry(self.sub_inn_frame1, font=('arial', 10, 'bold'), bd=4, insertwidth=2,textvariable=VfName)
+        self.fName_entry =Entry(self.sub_inn_frame1, font=('arial', 10, 'bold'), bd=4, insertwidth=2,textvariable=self.VfName)
         self.fName_entry.grid(row=1, column=1, padx=10)
 
         self.lName = Label(self.sub_inn_frame1, text = 'Surname.', font=('arial', 11, 'bold'))
         self.lName.grid(row=2, column=0, sticky=W,pady=10)
-        self.lName_entry =Entry(self.sub_inn_frame1, font=('arial', 10, 'bold'), bd=4, insertwidth=2,textvariable=VlName)
+        self.lName_entry =Entry(self.sub_inn_frame1, font=('arial', 10, 'bold'), bd=4, insertwidth=2,textvariable=self.VlName)
         self.lName_entry.grid(row=2, column=1, padx=10)
 
         self.addr = Label(self.sub_inn_frame1, text = 'Address.', font=('arial', 11, 'bold'))
         self.addr.grid(row=3, column=0, sticky=W,pady=10)
-        self.addr_entry =Entry(self.sub_inn_frame1, font=('arial', 10, 'bold'), bd=4, insertwidth=2,textvariable=Vaddr)
+        self.addr_entry =Entry(self.sub_inn_frame1, font=('arial', 10, 'bold'), bd=4, insertwidth=2,textvariable=self.Vaddr)
         self.addr_entry.grid(row=3, column=1, padx=10)
 
         self.pCode = Label(self.sub_inn_frame1, text = 'Postcode', font=('arial', 11, 'bold'))
         self.pCode.grid(row=4, column=0, sticky=W, pady=10)
-        self.pCode_entry =Entry(self.sub_inn_frame1, font=('arial', 10, 'bold'), bd=4, insertwidth=2,textvariable=VpCode)
+        self.pCode_entry =Entry(self.sub_inn_frame1, font=('arial', 10, 'bold'), bd=4, insertwidth=2,textvariable=self.VpCode)
         self.pCode_entry.grid(row=4, column=1, padx=10)
 
         self.telNo = Label(self.sub_inn_frame1, text ='Telephone No.', font=('arial', 11, 'bold'))
         self.telNo.grid(row=5, column=0, sticky=W,pady=10)
-        self.telNo_entry =Entry(self.sub_inn_frame1, font=('arial', 10, 'bold'), bd=4, insertwidth=2,textvariable=VtelNo)
+        self.telNo_entry =Entry(self.sub_inn_frame1, font=('arial', 10, 'bold'), bd=4, insertwidth=2,textvariable=self.VtelNo)
         self.telNo_entry.grid(row=5, column=1, padx=10)
             #-------------------------------------------------#
 
-        self.submitbtn1 = Button(self.sub_inn_frame1, text='Submit', font=('arial',12,'bold'),
-                                 width=10).grid(column=0, sticky=SW, pady=170, row=6)
+        self.submitbtn1 = Button(self.sub_inn_frame1, text='Submit',font=('arial',12,'bold'),
+                                 width=10,command= self.submit).grid(column=0, sticky=SW, pady=170, row=6)
         self.submitbtn2 = Button(self.sub_inn_frame1, text='Reset', font=('arial', 12, 'bold'),
                                  width=10).grid(column=1,sticky=SE,pady=170,row=6)
 
+        #===================================Function Declaration====================================#
+
+    def submit(self):
+            self.textReceipt.insert(END, "\t" + self.VrefNo.get())
 
 
 
